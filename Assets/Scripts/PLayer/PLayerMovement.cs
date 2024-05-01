2ab5e65297d33cd8cf7c56cebf9cb62f48085bd4
mermaiddspp300102 <tranducminhquan01@gmail.com>
2024-05-01 21:40:03 +0700

    1/5

---
+++ Assets/Scripts/PLayer/PLayerMovement.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    public float KBForce;
+    public float KBCounter;
+    public float KBTotalTime;
+    public bool KnockFromRight;
+
+    private bool doubleJump;
+    [SerializeField] private LayerMask jumpableGround;
+
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
+    private enum MovementState { idle, running, jumping, falling }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+
+        if (KBCounter <= 0)
+        {
+            rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        }
+        else
+        {
+            if (KnockFromRight == true)
+            {
+                rb.velocity = new Vector2(-KBForce, KBForce);
+            }
+            if (KnockFromRight == false)
+            {
+                rb.velocity = new Vector2(KBForce, KBForce);
+            }
+
+            KBCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded() || doubleJump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                doubleJump = !doubleJump;
+            }
+        }
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+        UpdateAnimationUpdate();
+    }
+
+    private void UpdateAnimationUpdate()
+    {
+        MovementState state;
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+}
---
+++ Assets/Scripts/Systems/ProceduralGeneration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeneration : MonoBehaviour
+{
+    public GameObject[] prefabsToPlace; // Mảng các Prefab cần xếp
+    public int numberOfPrefabsToPlace; // Số lượng Prefab cần xếp
+    public Vector2Int gridSize; // Kích thước của grid
+    public float spacing = 1f; // Khoảng cách giữa các Prefab
+    public Transform gridOrigin; // Gốc của grid
+
+    void Start()
+    {
+        PlacePrefabsRandomly();
+    }
+
+    void PlacePrefabsRandomly()
+    {
+        for (int i = 0; i < numberOfPrefabsToPlace; i++)
+        {
+            // Chọn một vị trí ngẫu nhiên trên grid
+            Vector2Int randomPosition = new Vector2Int(Random.Range(0, gridSize.x), Random.Range(0, gridSize.y));
+            Vector3 spawnPosition = gridOrigin.position + new Vector3(randomPosition.x * spacing, 0f, randomPosition.y * spacing);
+
+            // Chọn một Prefab ngẫu nhiên từ mảng Prefabs
+            GameObject randomPrefab = prefabsToPlace[Random.Range(0, prefabsToPlace.Length)];
+
+            // Xây dựng một bản sao của Prefab và đặt nó tại vị trí ngẫu nhiên
+            GameObject newObject = Instantiate(randomPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
